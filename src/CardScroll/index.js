1b18582d338275f12e3ecda020acd05c5260fc73
Warren Whipple <w@warrenwhipple.com>
2018-10-19 12:22:57 -0400

    Fix CardScroll scrollbar hiding

--- src/CardScroll/index.js
+++ src/CardScroll/index.js
@@ -3,9 +3,11 @@ import './index.css';
 
 const CardScroll = props => (
   <div className="CardScroll">
-    <div className="CardScroll__leadingSpacer" />
-    {props.children}
-    <div className="CardScroll__trailingSpacer" />
+    <div className="CardScroll__inner">
+      <div className="CardScroll__leadingSpacer" />
+      {props.children}
+      <div className="CardScroll__trailingSpacer" />
+    </div>
   </div>
 );
 
